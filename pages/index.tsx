974719f419cd1aab8ffd3f4ed8625c56b2432c8b
Samantha Seng <seng.samanthas@gmail.com>
2022-09-07 21:01:51 +0200

    :wrench: Fix indent type in Prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,18 @@ import Head from 'next/head';
 import Navbar from '../components/navbar';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&family=Roboto:wght@400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 
-      <Navbar />
-    </div>
-  );
+			<Navbar />
+		</div>
+	);
 };
 
 export default Home;
