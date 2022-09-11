5aba8d11cdeac8afd26604225823267e228800fc
Samantha Seng <seng.samanthas@gmail.com>
2022-09-11 15:48:35 +0200

    :truck: Move head in a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Navbar from '../components/navbar';
+import HeadComponent from '../components/head';
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<Head>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&family=Roboto:wght@400;500;700&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
+		<>
+			<HeadComponent />
 
 			<Navbar />
-		</div>
+		</>
 	);
 };
 
