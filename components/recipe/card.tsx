aac05755a760affa623b2bc6faea30874853f21d
Samantha Seng <samantha.seng@airweb.fr>
2023-01-24 18:58:42 +0100

    Revert ":recycle: Use tailwind for style"
    This reverts commit 400c1a4295bb88fa8c909c0e5f571b80902b616c.

--- components/recipe/card.tsx
+++ components/recipe/card.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Recipe } from '../../interfaces/Recipe';
+import { Card, CardContent, CardHeader, CardMedia } from '@material-ui/core';
+import styles from './card.module.css';
 import Link from 'next/link';
 
 export default function RecipeCard(props: { recipe: Recipe }) {
@@ -8,29 +10,35 @@ export default function RecipeCard(props: { recipe: Recipe }) {
 
 	return (
 		<Link href={`/recipes/${slug}`}>
-			<div className='overflow-hidden rounded-md cursor-pointer w-60 shadow-xl m-[10px] mb-5'>
-				<div className={`bg-[url("${image ? image : '/images/default-image.jpg'}")] w-full bg-center bg-cover h-72`}></div>
+			<Card className={styles.card}>
+				<CardMedia
+					component="img"
+					image={image ?? '/images/default-image.jpg'}
+					alt={name}
+					className={styles.image}
+				/>
 
-				<div className='flex justify-around py-3'>
-					<div className='flex items-center'>
+				<CardContent className={styles.durationWrapper}>
+					<div className={styles.duration}>
 						<img
 							src="./icons/balloon-whisk.svg"
-							className='w-5 mr-1'
+							className={styles.icon}
 							alt="Durée de préparation"
 						/>
-						<span className='font-serif text-sm uppercase'>{preparationDuration} min</span>
+						<span>{preparationDuration} min</span>
 					</div>
-					<div className='flex items-center'>
+					<div className={styles.duration}>
 						<img
 							src="./icons/kitchen-stove.svg"
-							className='w-5 mr-1'
+							className={styles.icon}
 							alt="Durée de cuisson"
 						/>
-						<span className='font-serif text-sm uppercase'>{cookingDuration} min</span>
+						<span>{cookingDuration} min</span>
 					</div>
-				</div>
-				<h2 className='font-serif font-medium text-center truncate text-stone-900'>{name}</h2>
-			</div>
+				</CardContent>
+
+				<CardHeader title={name} noWrap className={styles.header} />
+			</Card>
 		</Link>
 	);
 }
